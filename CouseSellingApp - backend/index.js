907f69bd4346c39f4704b292e0c3f9caa05285d2
suraj <surajnegi502sn@gmail.com>
2025-04-13 10:37:38 +0530

    port updated

--- CouseSellingApp - backend/index.js
+++ CouseSellingApp - backend/index.js
@@ -3,7 +3,7 @@ const express =require("express");
 const app=express();
 const mongoose=require("mongoose");
 const cors= require("cors")
-
+const port=process.env.PORT||3000
 const {userRouter} = require("./routes/user")
 const {courseRouter}=require("./routes/course");
 const {adminRouter}=require("./routes/admin");
@@ -15,6 +15,6 @@ app.use("/admin/v1",adminRouter);
 app.use("/course/v1",courseRouter);
 async function main (){
     await mongoose.connect(process.env.MONGO_URL)
-app.listen(3000);
+app.listen(port);
 }
 main();
